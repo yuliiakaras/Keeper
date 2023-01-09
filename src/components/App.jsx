b9197ca586587ded37c65a33bfa331f34095411d
yuliiakaras <yuliiakaras03@gmail.com>
2023-01-09 16:51:00 +0200

    restructure components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,35 +2,19 @@ import React, {useState} from 'react';
 import Header from "./Header.jsx";
 import Footer from "./Footer.jsx";
 import Note from "./Note.jsx";
-// import notes from "../notes.js"
+
 import CreateArea from "./CreateArea.jsx"
 function App () {
-	const [item, setItem] = useState({
-		title: "",
-		content: ""
-	});
+	
 	const [itemArr, setItemArr] = useState([]);
 
-	function addItem(event) {
+	function addItem(item) {
 		setItemArr((prevItems) => {
 			return [...prevItems, item]
 		})
-
-		console.log(itemArr);
-		setItem("");
-		event.preventDefault();
 	}
-
-	function handleChange(event) {
-		const {name, value} = event.target;
-		setItem((prevValue) => {
-			return {...prevValue,
-						[name]: value}
-		});
-	}
-
+	
 	function deleteItem(id) {
-		console.log(id)
 		setItemArr((prevItems) => {
 			return prevItems.filter((item, index) => {
 				return index !== id;
@@ -41,7 +25,6 @@ function App () {
 	return <div>
 		<Header />
 		<CreateArea 
-		 handleChange={handleChange}
 		 addItem={addItem}
 		 />
 		{itemArr.map((item, index) => 
--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -2,13 +2,34 @@ import React, {useState} from "react";
 
 function CreateArea(props) {
 
+const [item, setItem] = useState({
+		title: "",
+		content: ""
+	});
+
+function handleChange(event) {
+		const {name, value} = event.target;
+		setItem((prevValue) => {
+			return {...prevValue,
+						[name]: value}
+		});
+	}
+
+function submitNote(event) {
+		props.addItem(item)
+		setItem({
+			title: "",
+		content: ""
+		});
+		event.preventDefault();
+	}
 
   return (
     <div>
       <form type="submit">
-        <input onChange={props.handleChange} name="title" placeholder="Title" />
-        <textarea onChange={props.handleChange} name="content" placeholder="Take a note..." rows="3" />
-        <button onClick={props.addItem}>Add</button>
+        <input onChange={handleChange} value={item.title} name="title" placeholder="Title" />
+        <textarea onChange={handleChange}  value={item.content} name="content" placeholder="Take a note..." rows="3" />
+        <button onClick={submitNote}>Add</button>
       </form>
     </div>
   );
